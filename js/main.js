d3bb5dacabcd02680c5a64bd1b4a737894b4969c
Nora Anzawi <nanzawi@gmail.com>
2025-06-25 21:00:30 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,35 +1,41 @@
 /*  js/main.js  */
+/* Runs after jQuery and the Tocify plugin have loaded */
 $(function () {
-  /* build tabsets (pandoc helper) */
-  if (typeof window.buildTabsets === 'function') {
-    window.buildTabsets('TOC');
-  }
 
-  /* initialise code-folding (pandoc helper) */
-  if (typeof window.initializeCodeFolding === 'function') {
-    window.initializeCodeFolding(true);
-  }
-
-  /* -------  TOCify  -------- */
-  $('div.section.toc-ignore')
-    .removeClass('toc-ignore')
-    .children('h1,h2,h3,h4,h5')
-    .addClass('toc-ignore');
-
-  var options = {
-    selectors:       'h1,h2',
-    theme:           'bootstrap3',
-    context:         '.toc-content',
+  /* ----------  Build Table-of-Contents  ---------- */
+  var toc = $("#TOC").tocify({
+    selectors:       "h1,h2",
+    theme:           "bootstrap3",
+    context:         ".toc-content",
     hashGenerator:   function (text) {
                        return text.replace(/[.\\/?&!#<>]/g, '')
                                   .replace(/\s/g, '_')
                                   .toLowerCase();
                      },
-    ignoreSelector:  '.toc-ignore',
+    ignoreSelector:  ".toc-ignore",
     scrollTo:        0,
     showAndHide:     true,
     smoothScroll:    true
-  };
+  }).data("toc-tocify");
 
-  $('#TOC').tocify(options).data('toc-tocify');
+  /* ----------  Code-folding or tabsets (if you use them)  ---------- */
+  if (typeof window.buildTabsets === "function") {
+    window.buildTabsets("TOC");
+  }
+  if (typeof window.initializeCodeFolding === "function") {
+    window.initializeCodeFolding(true);
+  }
+
+  /* ----------  Portfolio filter buttons  ---------- */
+  $("#portfolio-button-container").on("click", "button", function () {
+    var section = $(this).data("portfolio-section");
+    $("#portfolio-button-container button").removeClass("active");
+    $(this).addClass("active");
+    if (section === "all") {
+      $("#portfolio-items > div").addClass("show");
+    } else {
+      $("#portfolio-items > div").removeClass("show")
+                                 .filter("." + section).addClass("show");
+    }
+  });
 });
